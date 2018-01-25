7404c6d3c86661550108e6a4154f217c895ded16
MACK Mathieu <mathieu.mack@econocom.com>
2018-01-25 15:06:49 +0100

    Add IReplicationChangeEventArgs fields

--- MvvX.Plugins.CouchBaseLite.Shared/MvvX.Plugins.CouchBaseLite.Shared/Sync/PlatformReplicationChangeEventArgs.cs
+++ MvvX.Plugins.CouchBaseLite.Shared/MvvX.Plugins.CouchBaseLite.Shared/Sync/PlatformReplicationChangeEventArgs.cs
@@ -1,4 +1,5 @@
-﻿using Couchbase.Lite;
+﻿using System;
+using Couchbase.Lite;
 using MvvX.Plugins.CouchBaseLite.Sync;
 
 namespace MvvX.Plugins.CouchBaseLite.Shared.Sync
@@ -20,9 +21,57 @@ namespace MvvX.Plugins.CouchBaseLite.Shared.Sync
             this.replication = replication;
         }
 
-        #endregion
-
         #region Implements
+
+        public IReplication Source
+        {
+            get
+            {
+                return replication;
+            }
+        }
+
+        public int ChangesCount
+        {
+            get
+            {
+                return replicationChangeEventArgs.ChangesCount;
+            }
+        }
+
+        public int CompletedChangesCount
+        {
+            get
+            {
+                return replicationChangeEventArgs.CompletedChangesCount;
+            }
+        }
+
+        public ReplicationStatus Status
+        {
+            get
+            {
+                return (ReplicationStatus)(int)replicationChangeEventArgs.Status;
+            }
+        }
+
+        public Exception LastError
+        {
+            get
+            {
+                return replicationChangeEventArgs.LastError;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return replicationChangeEventArgs.Username;
+            }
+        }
+
+        #endregion
 
         #endregion
     }
